6ed9024da8c5dca7c18546beef4838e18efc0b1a
Miroslav Juhy Juhos <juhy@rezatyrakety.cz>
2017-08-27 10:09:32 +0200

    Init new version

---
+++ brunch-config.js
@@ -0,0 +1,15 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: 'app.js'
+  },
+  stylesheets: {
+    joinTo: 'app.css'
+  },
+
+  plugins: {
+    sass: {
+      modules: true
+    }
+  }
+};
